2862710d2a74f6f732cc7021a63eda33f9b31b87
Ken Rimple <krimple@chariotsolutions.com>
2015-04-27 16:58:18 -0400

    initial

---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             'newdev'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures .'
+long_description 'Installs/Configures .'
+version          '0.1.0'
+
+
+depends 'mysql', '~> 6.0'
---
+++ recipes/default.rb
@@ -0,0 +1,17 @@
+#
+# Cookbook Name:: newdev
+# Recipe:: default
+#
+# Copyright (C) 2015 YOUR_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+mysql_service 'default' do
+  version '5.5'
+  bind_address '0.0.0.0'
+  port '3306'  
+  data_dir '/data'
+  initial_root_password 'Ch4ng3me'
+  action [:create, :start]
+end
